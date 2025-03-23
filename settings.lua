bc5cac42277cb113ae79292d9d8175987a747da9
Nicholas Gower <nicholasgower01@gmail.com>
2025-03-22 22:25:01 -0700

    other: Reduced default flow rate multiplier to 5, to address issues with uneven fluid distribution in throttled thrusters.

--- settings.lua
+++ settings.lua
@@ -5,8 +5,8 @@ data:extend{
         type = "double-setting",
         setting_type = "startup",
         name = "utility-constant-max-fluid-flow-multiplier",
-        default_value = 20,
+        default_value = 5,
         min = 1,
-        max = 50,
+        max = 20,
     }
 }
